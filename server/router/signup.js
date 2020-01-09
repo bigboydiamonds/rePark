432d60420a30b358f64649e34f8567681850b5e1
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2020-01-08 17:12:52 -0800

    latest working version, can update pins and see within database 512 pm

--- server/router/signup.js
+++ server/router/signup.js
@@ -8,16 +8,8 @@ const cookieController = require('../controllers/cookieController');
 
 signup.post('/', userController.createUser, cookieController.setCookie, (req, res) => {
 	const { phone } = req.body;
-<<<<<<< HEAD
-    phoneNum = Number(phone);
-	res.status(200)
-			.cookie('id', phoneNum, {httpOnly: false})
-			.json(phoneNum)
-			// .cookie('id', phoneNum, {httpOnly: false});
-=======
   phoneNum = Number(phone);
-	res.status(200).json(phoneNum);
->>>>>>> b6c1f7107f7b2728b26750639afe82551fd79ae4
+	res.status(200).json(res.locals.userID);
 });
 
 signup.put('/', userController.updateUserCar,
