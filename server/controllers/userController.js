f26535cef564216a722b45ec36f75aea4135a470
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2020-01-08 20:25:13 -0800

    time works now bruhs!

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -33,87 +33,6 @@ userController.createUser = async (req, res, next) => {
     next();
   }
 
-/*
-const userController = {
-
-  //Create user controller
-  createUser(req, res, next) {
-    const { name, phone, pass } = req.body;
-    const phoneNum = Number(phone);
-    const newUser = {
-      name: name,
-      phone: phoneNum,
-      pass: pass
-    }
-
-    User.create(newUser)
-      .then(userDoc => {
-        res.locals.user = userDoc;
-        return next();
-      })
-      .catch(err => {
-        return next({
-          log: 'Express error handler caught user create error',
-          status: 400,
-          message: { err: 'An error occurred' },
-        });
-      });
-  },
-
-  //verify user controller
-  verifyUser(req, res, next) {
-    const { phone, pass } = req.body;
-    User.findOne({ phone: phone })
-      .exec()
-      .then(userDoc => {
-        if (!userDoc) {
-          res.locals.auth = false;
-          return next();
-        }
-        if (userDoc.pass === req.body.pass) {
-          res.locals.auth = true;
-          res.locals.id = userDoc._id;
-          return next();
-        }
-        res.locals.auth = false;
-        return next();
-      })
-      .catch(err => {
-        res.status(401).json({ 'Error': err });
-        return next({
-          log: 'ERROR: userController verifyUser error',
-          status: 400,
-          message: { err: 'An error occurred' },
-        });
-      });
-  },
-
-  updateUserCar(req, res, next) {
-    const { car_make, car_model, car_color } = req.body.car;
-    const { id } = req.body;
-    User.findOneAndUpdate({ _id: id },
-      {
-        car: {
-          car_make: car_make,
-          car_model: car_model,
-          car_color: car_color
-        }
-      },
-      { new: true }, (err, updatedDoc) => {
-        if (err) {
-          return next({
-            log: 'Express error handler caught car update error',
-            status: 400,
-            message: { err: 'An error occurred' },
-          });
-        };
-        res.locals.successfulSignup = true;
-        return next();
-      })
-  }
-}
-
-*/
 userController.verifyUser = async (req, res, next)=>{
   const { phone, pass } = req.body;
   const phoneNum = Number(phone);
@@ -146,7 +65,7 @@ userController.updateUserCar = (req, res, next) => {
     SET car_make = '${req.body.car.car_make}',
         car_model = '${req.body.car.car_model}',
         car_color = '${req.body.car.car_color}'
-    WHERE phone_number = ${id}
+    WHERE _id = ${id}
   `
 
   db.query(text)
